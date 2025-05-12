c06fbbba8bc31be65ce69940552d57b55e2dc3b8
adptra01 <m.a.syaputra.94@gmail.com>
2025-05-12 21:58:56 +0700

    fix(logout): perbaiki fungsi logout

--- resources/views/components/navigations/admin-header.blade.php
+++ resources/views/components/navigations/admin-header.blade.php
@@ -1,17 +1,17 @@
 <?php
 
-
+use function Livewire\Volt\{state};
 
 $logout = function () {
     Auth::logout();
-    $this->redirect('/');
+    $this->redirect("/");
 };
 ?>
 
-<div>
+@volt
     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
         <div class="message-body">
-            <a href="{{ route('account.auth') }}" class="d-flex align-items-center gap-2 dropdown-item">
+            <a href="{{ route("account.auth") }}" class="d-flex align-items-center gap-2 dropdown-item">
                 <i class="ti ti-user fs-6"></i>
                 <p class="mb-0 fs-3">Akun Profile</p>
             </a>
@@ -21,4 +21,4 @@ $logout = function () {
             </button>
         </div>
     </div>
-</div>
+@endvolt
